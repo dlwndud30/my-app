5876625ef51e928f27f2b832246a0726794efdae
JuYeong <juyeong3022@osstem.com>
2023-10-12 17:28:10 +0900

    test 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
 import "./App.css";
+import Test from "./components/Test";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>hello</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Test></Test>
     </div>
   );
 }
---
+++ src/components/Test.js
@@ -0,0 +1,13 @@
+function Test() {
+  return (
+    <div class="container text-center">
+      <div class="row">
+        <div class="col">Column</div>
+        <div class="col">Column</div>
+        <div class="col">Column</div>
+      </div>
+    </div>
+  );
+}
+
+export default Test;
